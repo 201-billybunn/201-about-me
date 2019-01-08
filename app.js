75efdbd7f3fef136b73c2c440c9612a50b1d1398
Billy Bunn <williammbunn@gmail.com>
2019-01-08 12:25:51 -0800

    added JS logic and CS styles

---
+++ app.js
@@ -0,0 +1,15 @@
+var kids = prompt('How many kids do you have?');
+console.log('kids:', kids);
+
+var guitars = prompt('How many guitars do you have?');
+console.log('guitars:', guitars);
+
+if (kids === '3' || kids === '4') {
+    alert('You are correct! Too many kids');
+}
+
+if (parseInt(kids) === 3 && parseInt(guitars) === 3) {
+    alert('yeah you are right');
+} else {
+    alert('you are wrong');
+}
